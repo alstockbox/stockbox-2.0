89fc33f863199db4f2729763521f3a5bbd41c427
alstockbox <alwatchbox@gmail.com>
2026-09-01 23:07:02 +0200

    feat(alpha): expose breakout intelligence engine

---
+++ src/lib/alpha/index.ts
@@ -0,0 +1,11 @@
+export { ALPHA_MODEL_VERSION, computeAlphaIntelligence } from "./engine";
+export type {
+  AlphaClassification,
+  AlphaDimensionScores,
+  AlphaHistoryPoint,
+  AlphaIntelligenceResult,
+  AlphaProbabilityCurve,
+  AlphaRisk,
+  AlphaSignalInput,
+  AlphaUpsideProbability,
+} from "./types";
