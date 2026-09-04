d4c6b2c8bfa80b9c80c807e8d2582ad5bff7b800
alstockbox <alwatchbox@gmail.com>
2026-09-04 22:02:34 +0200

    test: stage semantic provider period alignment

---
+++ src/lib/data/period-alignment.ts
@@ -0,0 +1,5 @@
+import type { FinancialPeriod } from "@/lib/analysis/types";
+
+export function periodsSemanticallyMatch(_left: FinancialPeriod, _right: FinancialPeriod): boolean {
+  return false;
+}
