73040dc50259043968a78c0a49be45acd30f5117
alstockbox <alwatchbox@gmail.com>
2026-09-05 18:47:20 +0200

    fix(growth): render video templates as static components

--- src/video/GrowthVideo.tsx
+++ src/video/GrowthVideo.tsx
@@ -1,9 +1,24 @@
 import type { GrowthCompositionProps } from "./render-adapter";
-import { selectGrowthTemplate } from "./templates/select-template";
+import { CompanyComparison } from "./templates/CompanyComparison";
+import { EducationalChecklist } from "./templates/EducationalChecklist";
+import { InvestorWarning } from "./templates/InvestorWarning";
+import { StockAnalysis } from "./templates/StockAnalysis";
+import { StockBoxDemo } from "./templates/StockBoxDemo";
 
 export function GrowthVideo(props: GrowthCompositionProps) {
-  const Template = selectGrowthTemplate(props.spec.template);
-  return <Template {...props} />;
+  switch (props.spec.template) {
+    case "stock_analysis":
+      return <StockAnalysis {...props} />;
+    case "investor_warning":
+      return <InvestorWarning {...props} />;
+    case "stockbox_demo":
+      return <StockBoxDemo {...props} />;
+    case "company_comparison":
+      return <CompanyComparison {...props} />;
+    case "educational_checklist":
+    default:
+      return <EducationalChecklist {...props} />;
+  }
 }
 
 GrowthVideo.displayName = "GrowthVideo";
